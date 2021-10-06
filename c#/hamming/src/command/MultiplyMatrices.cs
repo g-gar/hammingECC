99b85402ec9b276296af1709053a3225e908d587
g-gar <g.gar.fern@hotmail.com>
2021-10-06 13:36:42 +0200

    refactorized imports, added default default modulus value for matrix multiplication and separated modulus operation expression from the aggregation expression

--- c#/hamming/src/command/MultiplyMatrices.cs
+++ c#/hamming/src/command/MultiplyMatrices.cs
@@ -9,12 +9,13 @@ namespace hamming.command{
             return execute(m1, m2, 10);
         }
 
-        public static int[,] execute(int[,] m1, int[,] m2, int modulus)
+        public static int[,] execute(int[,] m1, int[,] m2, int modulus = 10)
         {
             return VisitMatrix.execute(m1.GetLength(0), m2.GetLength(1), (i, j) =>
                 Enumerable.Range(0, m1.GetLength(1)-1)
                     .Select(k => m1[i, k] * m2[k, j])
-                    .Aggregate(0, (a, b) => (a + b) % modulus));
+                    .Aggregate(0, (a, b) => (a + b), e => e % modulus)
+            );
         }
     }
 }
